1d0fc5f0f0cce2168307ddfd30f72b16f8277ef7
Fellipe Lorram <fellipelorram@gmail.com>
2022-01-21 14:38:49 -0300

    adding .

--- src/UseCases/CreateUser/CreateUserValidations.ts
+++ src/UseCases/CreateUser/CreateUserValidations.ts
@@ -7,9 +7,9 @@ export class CreateUserValidations {
 
     if (!validator.isEmail(email)) throw new Error('Invalid email.');
 
-    if (password.length < 8 || password.length > 55) throw new Error('Invalid password. Password have to be at least 8 characters and less than 55 characters');
+    if (password.length < 8 || password.length > 55) throw new Error('Invalid password. Password have to be at least 8 characters and less than 55 characters.');
 
-    if (name.length < 8 || name.length > 55) throw new Error('Invalid name. name have to be at least 8 characters and less than 55 characters');
+    if (name.length < 8 || name.length > 55) throw new Error('Invalid name. name have to be at least 8 characters and less than 55 characters.');
 
   }
 }
